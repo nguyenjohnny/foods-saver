cb882fb4c8c546cd799bec2924d46a4268ecdee6
Johnny Nguyen <43325262+nguyenjohnny@users.noreply.github.com>
2018-10-04 13:00:08 -0700

    Create blog.py

---
+++ blog.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, url_for 
+from forms import RegistrationForm, LoginForm
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'b29843ff36cb11b71ba8246ce6fc80ae'
+
+
+posts = [
+		{
+		'author': 'Johnny Nguyen',
+		'title': 'Blog post 1',
+		'content': 'First post content',
+		'date_posted': 'Oct 3, 2018',
+	},
+	{
+		'author': 'Jane Nguyen',
+		'title': 'Blog post 2',
+		'content': 'second post content',
+		'date_posted': 'Oct 4, 2018',
+	},
+]
+
+@app.route("/")    
+@app.route("/home")  
+def home():
+    return render_template('home.html', posts=posts)  
+
+
+@app.route("/about")  
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/register')
+def register():
+	form = RegistrationForm()
+	return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='login', form=form)
+ 
+if __name__ == '__main__':
+	app.run(debug=True)
